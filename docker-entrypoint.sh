0a013cea919489b79c5e24efc8c20bd096ce2d8e
Ioannis Angelakopoulos <ioangel@gmail.com>
2019-12-24 19:49:29 +0200

    Migrates form glassfish password aliases to env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,12 @@ if ! getent passwd "$USER_NAME" > /dev/null; then
     chown -R "$USER_NAME":"$USER_NAME" /glassfish4
 fi
 
+GF_CONFIG=/glassfish4/glassfish/domains/domain1/config
+if [ ! -f "$GF_CONFIG/domain.xml" ] ; then
+  cp "$GF_CONFIG"/domain.before-sed.xml "$GF_CONFIG"/domain.xml
+  gosu "$USER_NAME" sed -i "s/MYSQL_PASS_PLACEHOLDER/$MYSQL_PASS/" "$GF_CONFIG"/domain.xml
+fi
+
 if [ "$1" = 'asadmin' ]; then
     exec gosu "$USER_NAME" "$@"
 fi
