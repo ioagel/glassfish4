8fe8af47106f96f13489dfd8f15c8c36fa4f9b96
Ioannis Angelakopoulos <ioagel@gmail.com>
2019-12-30 19:26:35 +0200

    Reverts image to user root

--- Dockerfile
+++ Dockerfile
@@ -5,26 +5,12 @@ MAINTAINER  Ioannis Angelakopoulos<ioagel@gmail.com>
 ENV \
   GF_REL=4.1.2-web \
   MJDBC=8.0.18 \
-  USER_NAME=glassfish \
-  USER_ID=10001 \
-  GOSU_VERSION=1.11 \
   PATH="${PATH}:/glassfish4/bin"
 
 RUN echo "deb http://deb.debian.org/debian buster contrib" > /etc/apt/sources.list.d/contrib.list
 RUN apt-get update && apt-get install -y --no-install-recommends gnupg dirmngr wget unzip ttf-mscorefonts-installer && rm -rf /var/lib/apt/lists/*
 
-# Kindly borrowed from official mysql image ;-)
-# add gosu for easy step-down from root
 RUN set -x && \
-  wget -O /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" && \
-  wget -O /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" && \
-  export GNUPGHOME="$(mktemp -d)" && \
-  gpg --batch --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 && \
-  gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu && \
-  gpgconf --kill all && \
-  rm -rf "$GNUPGHOME" /usr/local/bin/gosu.asc && \
-  chmod +x /usr/local/bin/gosu && \
-  gosu nobody true && \
   cp -a /usr/share/fonts/truetype/msttcorefonts/* /usr/share/fonts/truetype && \
   wget http://download.oracle.com/glassfish/4.1.2/release/glassfish-${GF_REL}.zip && \
   wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MJDBC}.zip && \
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-if ! getent passwd "$USER_NAME" > /dev/null; then
-    useradd -d /glassfish4 -M -s /usr/sbin/nologin -u "$USER_ID" "$USER_NAME"
-    chown -R "$USER_NAME":"$USER_NAME" /glassfish4
-fi
-
 GF_CONFIG=/glassfish4/glassfish/domains/domain1/config
 if [ ! -f "$GF_CONFIG/domain.xml.configured" ] ; then
-  gosu "$USER_NAME" cp "$GF_CONFIG"/domain.tpl.xml "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/MYSQL_PASS/$MYSQL_PASS/" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/MYSQL_HOST/$MYSQL_HOST/" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/MYSQL_PORT/$MYSQL_PORT/" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/MYSQL_DB/$MYSQL_DB/" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/MYSQL_USER/$MYSQL_USER/" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" sed -i "s/APP_INSIGHTS_AGENT_VER/$APP_INSIGHTS_AGENT_VER/g" "$GF_CONFIG"/domain.xml
-  gosu "$USER_NAME" touch "$GF_CONFIG"/domain.xml.configured
-fi
-
-if [ "$1" = 'asadmin' ]; then
-    exec gosu "$USER_NAME" "$@"
+  cp "$GF_CONFIG"/domain.tpl.xml "$GF_CONFIG"/domain.xml
+  sed -i "s/MYSQL_PASS/$MYSQL_PASS/" "$GF_CONFIG"/domain.xml
+  sed -i "s/MYSQL_HOST/$MYSQL_HOST/" "$GF_CONFIG"/domain.xml
+  sed -i "s/MYSQL_PORT/$MYSQL_PORT/" "$GF_CONFIG"/domain.xml
+  sed -i "s/MYSQL_DB/$MYSQL_DB/" "$GF_CONFIG"/domain.xml
+  sed -i "s/MYSQL_USER/$MYSQL_USER/" "$GF_CONFIG"/domain.xml
+  sed -i "s/APP_INSIGHTS_AGENT_VER/$APP_INSIGHTS_AGENT_VER/g" "$GF_CONFIG"/domain.xml
+  touch "$GF_CONFIG"/domain.xml.configured
 fi
 
 exec "$@"
